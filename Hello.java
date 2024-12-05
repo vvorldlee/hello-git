ee8e7e1e3113c8e1dece791a191f40e4cbd26136
Sesang Lee <sesang7899@gmail.com>
2024-12-05 17:46:56 +0000

    C4 : modify Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, world from GIT!");
         System.out.println("Change after First Commit");
+        System.out.println("Changes after Pushing to remote");
     }
 }
